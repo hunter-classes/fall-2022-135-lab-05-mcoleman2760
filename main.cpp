e48c4ef3d7fbe6d3cbad8315b2e705f257b943fa
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-10-07 01:54:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,23 @@
 
 int main()
 {
+  int main() {
+  std::cout << isDivisibleBy(100, 25) << std::endl ;
+  std::cout << isDivisibleBy(35, 17) << std::endl ;
+  std::cout << isPrime(17) << std::endl;
+  std::cout << isPrime(49) << std::endl;
+  std::cout << nextPrime(14) << std::endl;
+  std::cout << nextPrime(17) << std::endl;
+  std::cout << countPrimes(10,20) << std::endl;
+  std::cout << countPrimes(1,12) << std::endl;
+  std::cout << isTwinPrime(17)  << std::endl;
+   std::cout << isTwinPrime(8)  << std::endl;
+   std::cout << nextTwinPrime(17)  << std::endl;
+   std::cout << nextTwinPrime(8)  << std::endl;
+  std::cout << largestTwinPrime(5,18)  << std::endl;
+  std::cout << largestTwinPrime(1,31)  << std::endl;
+  std::cout << largestTwinPrime(14,16)  << std::endl;
+  
+}
   return 0;
 }
