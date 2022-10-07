de46ecbfc9a988d5e231264c6d666546d33c2399
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-10-06 23:31:20 -0400

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
 #include "funcs.h"
+#include <string>
 
-// add functions here
+bool isDivisibleBy(int n, int d) {
+  bool s ;
+  if (n % d == 0) {
+   s = true;
+    return s;
+  }
 
+  else if (n % d != 0) {
+     s = false;
+    return s;
+  }
+}
+
+   bool isPrime(int n){
+      bool s = true ;
+
+     if (n <=1) {
+      
+       s = false;
+     }
+    for (int i = 2; i <= n/2; i++){
+      if (n % i == 0) {
+        
+         s = false;
+      }
+     
+      
+     
+    }
+     return s;
+     }
+
+int nextPrime(int n){
+ 
+  if (n <=1){
+    
+   return 2;
+   }
+
+  int prime = n;
+    bool next = false;
+  
+while (!next) {
+        prime++;
+ 
+        if (isPrime(prime))
+            next = true;
+    }
+ 
+    return prime;
+}
